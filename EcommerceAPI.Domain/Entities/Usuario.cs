67e2afd27050596713a2046f67d9bef17b7fa2d2
benicio <beniciobrandao@hotmail.com>
2025-03-06 08:49:48 -0300

    Ajustando a propriedade Nome da entidade Usuario

--- EcommerceAPI.Domain/Entities/Usuario.cs
+++ EcommerceAPI.Domain/Entities/Usuario.cs
@@ -5,7 +5,7 @@ namespace EcommerceAPI.Domain.Entities;
 public class Usuario
 {
     public int Id {  get; set; }
-    public string Nome { get; set; } = string.Empty;
+    public string Name { get; set; } = string.Empty;
     public string Email { get; set; } = string.Empty;
     public string SenhaHash { get; set; } = string.Empty;
     public PerfilUsuario Perfil {  get; set; } = PerfilUsuario.Cliente;
