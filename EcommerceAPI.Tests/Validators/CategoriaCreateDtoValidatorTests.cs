dd38bf14105082ee56f239e759510c87e6c7b539
benicio <beniciobrandao@hotmail.com>
2025-03-18 10:47:11 -0300

    Criando teste de unidade para Categoria

---
+++ EcommerceAPI.Tests/Validators/CategoriaCreateDtoValidatorTests.cs
@@ -0,0 +1,57 @@
+﻿using EcommerceAPI.Application.Validators;
+using EcommerceAPI.DTOs;
+using FluentAssertions;
+using FluentValidation.TestHelper;
+
+namespace EcommerceAPI.Tests.Validators;
+public class CategoriaCreateDtoValidatorTests
+{
+    private readonly CategoriaCreateDtoValidator _validator;
+
+    public CategoriaCreateDtoValidatorTests()
+    {
+        _validator = new CategoriaCreateDtoValidator();    
+    }
+
+    [Fact]
+    public void Deve_Passar_Quando_Dados_Forem_Validos()
+    {
+        var dto = new CategoriaCreateDto
+        {
+            Nome = "Perfumaria"
+        };
+
+        var result = _validator.TestValidate(dto);
+
+        result.IsValid.Should().BeTrue();
+    }
+
+    [Fact]
+    public void Deve_Falhar_Quando_Nome_For_Nulo()
+    {
+        var dto = new CategoriaCreateDto
+        {
+            Nome = null!,
+        };
+
+        var result = _validator.TestValidate(dto);
+
+        result.ShouldHaveValidationErrorFor(categoria => categoria.Nome)
+            .WithErrorMessage("O nome da categoria é obrigatório");
+    }
+
+    [Theory]
+    [InlineData("")]
+    [InlineData("   ")]
+    public void Deve_Falhar_Quando_Nome_For_Vazio_Ou_Espacos(string nomeInvalido)
+    {
+        var dto = new CategoriaCreateDto
+        {
+            Nome = nomeInvalido
+        };
+
+        var result = _validator.TestValidate(dto);
+
+        result.ShouldHaveValidationErrorFor(categoria => categoria.Nome);
+    }
+}
