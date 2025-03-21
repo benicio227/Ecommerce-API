f54077475ac3e27a637e8d09454603d1bf75e3ec
benicio <beniciobrandao@hotmail.com>
2025-03-21 10:33:53 -0300

    Modifiquei o nome da propriedade de Senha para SenhaHash

--- EcommerceAPI.Application/DTOs/UsuarioCreateDto.cs
+++ EcommerceAPI.Application/DTOs/UsuarioCreateDto.cs
@@ -6,7 +6,7 @@ public class UsuarioCreateDto
 {
     public string Name { get; set; } = string.Empty;
     public string Email {  get; set; } = string.Empty;
-    public string Senha {  get; set; } = string.Empty;
+    public string SenhaHash {  get; set; } = string.Empty;
     public PerfilUsuario Perfil { get; set; } = PerfilUsuario.Cliente;
     public string? Telefone {  get; set; }
 }
