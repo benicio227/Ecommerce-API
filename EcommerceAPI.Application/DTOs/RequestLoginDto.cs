650ea24cc391544cb99ef309db8b5725fe8ce066
benicio <beniciobrandao@hotmail.com>
2025-03-22 08:52:40 -0300

    Cria dtos de request e response para login

---
+++ EcommerceAPI.Application/DTOs/RequestLoginDto.cs
@@ -0,0 +1,6 @@
+﻿namespace EcommerceAPI.Application.DTOs;
+public class RequestLoginDto
+{
+    public string Email { get; set; } = string.Empty;
+    public string Senha { get; set; } = string.Empty;
+}
